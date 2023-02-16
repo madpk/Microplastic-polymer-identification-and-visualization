57e16c2814d2b97eb0de37d1b1a3449077f14bed
Madhuraj PK <89763299+madpk@users.noreply.github.com>
2023-02-16 17:41:31 +0530

    Update Microplastic polymer identification.R

--- Microplastic polymer identification.R
+++ Microplastic polymer identification.R
@@ -1,5 +1,7 @@
 #Load the library
 library(OpenSpecy)
+library(tidyverse)
+
 get_lib()
 spec_lib <- load_lib()
 
@@ -31,6 +33,7 @@ raman_proc <- lapply(raman_adj,smooth_intens)
 raman_proc<-lapply(raman_proc,subtr_bg)
 
 #function to cross refer peaks (.txt files)
+
 mp_spec<-function(x){
   match_spec(x,library = spec_lib, which = "raman")
 }
@@ -40,4 +43,30 @@ match_list<-lapply(raman_proc, mp_spec)
 
 #Top 6 suspected polymers
 match_list_head<-lapply(match_list,head) 
+
+####Visualization of Peaks
+
+#Function to plot .txt files
+
+plotdata_cor <- function(x){
+  ggplot(data = x, aes(x=wavenumber, y=intensity, color="blue"))+
+    geom_line(color="red",size=0.4)+ xlim(1000, 3500)+
+    theme_classic()+
+    xlab(expression(paste("Raman shift (", cm^-1,")")))+
+    ylab("Intensity")+ theme(axis.title.x = element_text(size = 12),
+                             axis.title.y = element_text(size = 12))
+  
+}
+
+
+#Plot the .txt files
+ggpeaks_cor<-lapply(mppeaks, plotdata_cor)
+
+
+#Save the plots
+lapply(names(ggpeaks_cor), 
+       function(x)ggsave(filename=paste(x,".png",sep=""), 
+                         plot=ggpeaks_cor[[x]],dpi=350,
+                         width = 20, height = 15, units = "cm"))
+
 
